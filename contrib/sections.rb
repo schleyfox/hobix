b8488a9f703b56799e06e2d677c4576efa22922b
William Morgan <wmorgan-rubyforge@masanjin.net>
2005-05-01 21:34:40 +0000

    - contrib/sections.rb: merged in most recent section map code.

--- contrib/sections.rb
+++ contrib/sections.rb
@@ -1,6 +1,8 @@
-## sections.rb -- Hobix sections plugin version 0.1, released 9/20/04
-## (c) 2004 William Morgan. This file is released under the GNU Public
-##     License.
+## sections.rb -- Hobix section map plugin
+## (c) 2004, 2005 William Morgan. This file is released under the GNU
+## Public License.
+##
+## Contributors: Frederick Ros, Eric Stewart, Jeremy Hinegardner
 ##
 ## USAGE: place this file in your $HOBIXROOT/lib directory. Then, in
 ## your $HOBIXROOT/hobix.yaml file (or by running 'hobix edit'), add
@@ -8,7 +10,6 @@
 ## 
 ## [...]
 ## required:
-## [...]
 ##   - sections
 ##
 ## Finally, add 'sidebar_section_list' to the 'sidebar_list' property
@@ -16,8 +17,7 @@
 ##
 ## hobix/out/quick: 
 ##   sidebar_list: 
-## [...]     
-##      - sidebar_section_list
+##      - sidebar_section_map
 ##
 ## Voila, your sidebar will display a section map when you next do a
 ## regen. Note that the default Hobix CSS doesn't indent lists, so the
@@ -29,55 +29,59 @@
 ##     padding:0px;
 ## }
 ##
-## Good luck. -- William <wmorgan-sidebar-hobix@masanjin.net>
+## Good luck. -- William <wmorgan-section_map-hobix@masanjin.net>
 
 class Hobix::Out::Quick
-  def section_list(entries)
+  def section_list(entries, topname="top level")
     counts = {}
-    entries.each do |path, mtime|
-      path = path.split("/")[0 ... -1]
-      counts[path] ||= 0
-      counts[path] += 1
+    entries.each do |entry|
+      path = (entry.id.split("/")[0 ... -1] || []) # nil in ruby 1.8.1 if path is ""
+      counts[path] = 1 + (counts[path] || 0)
     end
 
     list = []; seen = {}
+    counts[[]] ||= 0 # force the root
     counts.sort.each do |path, count|
-      path[0 ... -1].inject("") do |s, x|
-        prefix = s + "/" + x
-        unless seen[prefix]
-          seen[prefix] = true
-          list.push [x, prefix, prefix.count('/'), 0]
+      if path == []
+        list.push [topname, "", 0, counts[path]]        
+      else
+        path.inject(".") do |s, x|
+          prefix = s + "/" + x
+          unless seen[prefix]
+            length = prefix.count '/'
+            list.push [x, prefix, length, (length == path.length ? count : 0)]
+            seen[prefix] = true
+          end
+          prefix
         end
-        prefix
       end
-      leafpath = "/" + path.join('/')
-      seen[leafpath] = true
-      list.push [path[-1], leafpath, path.length, count]
     end
     list
   end
 
-  def sidebar_section_list_erb
-    %q{
+  def sidebar_section_map_erb
+  %q{
     <div class="sidebarBox">
     <h2 class="sidebarTitle">Sections</h2>
-    <% curlev = 0 %>
+    <% curlev = -1 %>
     <% section_list(weblog.storage.find).each do |name, path, lev, num| %>
       <% if (lev > curlev) %>
         <% (lev - curlev).times do %> <ul> <% end %>
-      <% elsif (curlev > lev) %>
-        <% (curlev - lev).times do %> </ul> <% end %>
+      <% else # less than or equal %>
+        </li>
       <% end %>
+      <% if (curlev > lev) %>
+        <% (curlev - lev).times do %> </ul></li> <% end %>
+      <% end %>
+
       <% curlev = lev %>
       <li>
-        <a href="<%= weblog.link %><%= path %>"><%= name %></a>
-        <% if num != 0 then %> (<%= num %>) <% end %>
-      </li>
+        <a href="<%= weblog.expand_path path %>"><%= name %></a><% if num != 0 then %>: <%= num %> <% end %>
     <% end %>
-    <% curlev.times do %>
-      </ul>
+    <% (curlev + 1).times do %>
+      </li></ul>
     <% end %>
     </div>
-    }
+  }
   end
 end
