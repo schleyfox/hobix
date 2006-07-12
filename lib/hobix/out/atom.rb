5b4c5d9b294c6647fd0d55bc3a4b6522539b8953
MenTaLguY <mental@rydia.net>
2006-07-12 01:52:45 +0000

    emit valid Atom 1.0

--- lib/hobix/out/atom.rb
+++ lib/hobix/out/atom.rb
@@ -39,38 +39,39 @@ class Atom < Hobix::BaseOutput
     end
     def load( file_name, vars )
         rssdoc = REXML::Document.new( <<EOXML )
-<feed version="0.3"
-  xmlns="http://purl.org/atom/ns#"
+<feed
+  xmlns="http://www.w3.org/2005/Atom"
   xmlns:dc="http://purl.org/dc/elements/1.1/"
   xml:lang="en">
     <title></title>
     <link rel="alternate" type="text/html" href="" />
-    <modified></modified>
-    <tagline></tagline>
+    <!--link rel="self" type="application/atom+xml" href="" /-->
+    <updated></updated>
+    <subtitle></subtitle>
     <id></id>
-    <generator url="http://hobix.com/" version="#{ Hobix::VERSION }">Hobix</generator>
-    <copyright></copyright>
+    <generator uri="http://hobix.com/" version="#{ Hobix::VERSION }">Hobix</generator>
+    <rights></rights>
 </feed>
 EOXML
         uri = vars[:weblog].link
         rssdoc << REXML::XMLDecl.new
         rssdoc.elements['/feed/title'].text = vars[:weblog].title
         rssdoc.elements['/feed/link'].attributes['href'] = vars[:weblog].link.to_s
-        rssdoc.elements['/feed/tagline'].text = vars[:weblog].tagline
-        rssdoc.elements['/feed/modified'].text = vars[:page].updated.strftime( "%Y-%m-%dT%H:%M:%SZ" )
+        rssdoc.elements['/feed/subtitle'].text = vars[:weblog].tagline
+        rssdoc.elements['/feed/updated'].text = vars[:page].updated.strftime( "%Y-%m-%dT%H:%M:%SZ" )
         rssdoc.elements['/feed/id'].text = "tag:#{ uri.host },#{ Time.now.year }:blog#{ uri.path }"
-        rssdoc.elements['/feed/copyright'].text = vars[:weblog].copyright || "None"
+        rssdoc.elements['/feed/rights'].text = vars[:weblog].copyright || "None"
         ( vars[:entries] || [vars[:entry]] ).each do |e|
             ele = REXML::Element.new 'entry'
             ele.extend XmlQuick
             ele.x( 'title', e.title )
             ele.x( 'link', nil, {'rel' => 'alternate', 'type' => 'text/html', 'href' => e.link } )
             ele.x( 'id', "tag:#{ uri.host },#{ Time.now.year }:blog#{ CGI.escape(uri.path) }entry#{ CGI.escape( "/#{ e.id }" ) }" )
-            ele.x( 'issued', e.created.strftime( "%Y-%m-%dT%H:%M:%SZ" ) )
-            ele.x( 'modified', (e.modified || e.created).strftime( "%Y-%m-%dT%H:%M:%SZ" ) )
+            ele.x( 'published', e.created.strftime( "%Y-%m-%dT%H:%M:%SZ" ) )
+            ele.x( 'updated', (e.modified || e.created).strftime( "%Y-%m-%dT%H:%M:%SZ" ) )
             ele.x( 'dc:subject', e.section_id )
             e.tags.each do |t|
-                ele.x( 'dc:subject', t )
+                ele.x( 'category', '', { 'term' => t, 'scheme' => "http://hobix.com/tags" } )
             end
             ele.x( 'summary', 
                 e.summary.to_html.gsub( /img src="\//, "img src=\"#{ vars[:weblog].link }/" ),
@@ -79,12 +80,12 @@ EOXML
             ele_auth = REXML::Element.new 'author'
             ele_auth.extend XmlQuick
             ele_auth.x( 'name', author['name'] )
-            ele_auth.x( 'url', author['url'] ) if author['url']
+            ele_auth.x( 'uri', author['url'] ) if author['url']
             ele_auth.x( 'email', author['email'] ) if author['email']
             ele << ele_auth
             ele.x( 'content',
                 e.content.to_html.gsub( /img src="\//, "img src=\"#{ vars[:weblog].link }/" ),
-                {'type' => 'text/html', 'mode' => 'escaped'} )
+                {'type' => 'html'} )
             rssdoc.elements['/feed'].add ele
         end
         rssdoc.to_s
